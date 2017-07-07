c24b754e3a358b26d960d7aad485fa6286715b5b
gamename <tennis.n.smith@gmail.com>
2017-07-07 09:21:56 -0500

    Renamed variables for more clarity

--- audit/audit.py
+++ audit/audit.py
@@ -39,13 +39,13 @@ logger.setLevel(logging.INFO)
 # this lambda function using the "KMS key" option in the lambda Advanced
 # settings section of the Configuration tab.
 #
-ENCRYPTED_HOOK_URL = "AQICAHj49HEE8m06MDGiHk8OqJLX6IypkhfdVc1rfWY5AULrSgEYAMIwOQh7L+oWOOhm4UsQAAAApzCBpAYJKoZIhvcNAQcGoIGWMIGTAgEAMIGNBgkqhkiG9w0BBwEwHgYJYIZIAWUDBAEuMBEEDPqLY0VrGynYTBnl5QIBEIBgNgMJgzx5yptjBk5dBBjC9DSJSAsLDd/uDle6r/dHa453czKeFhr8bX5ML5S4NF+UN6ysmqzYeQdK0HHTdIheszvWLw76c3gpfzu9UXKaXk3B8/FspfQM72siyzZHWp19"
-DECRYPTED_HOOK_URL = boto3.client('kms').decrypt(CiphertextBlob=b64decode(ENCRYPTED_HOOK_URL))['Plaintext']
-HOOK_URL = "https://%s" % DECRYPTED_HOOK_URL
-ENCRYPTED_USER = "AQICAHj49HEE8m06MDGiHk8OqJLX6IypkhfdVc1rfWY5AULrSgHZ0ERc2JNL1wKY/wYE9fKSAAAAcDBuBgkqhkiG9w0BBwagYTBfAgEAMFoGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMH52GAgIeBfjMPDKtAgEQgC3jawZ/yi9KRFlUXfyB+uwZleA/gq38j1oCqCWktrasXAgw9OB3SpNcvhyCVms="
-DECRYPTED_USER = boto3.client('kms').decrypt(CiphertextBlob=b64decode(ENCRYPTED_USER))['Plaintext']
-ENCRYPTED_PASSWORD = "AQICAHj49HEE8m06MDGiHk8OqJLX6IypkhfdVc1rfWY5AULrSgEhzJYYnEQfpE2j0J2LlMQpAAAAZzBlBgkqhkiG9w0BBwagWDBWAgEAMFEGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMH+wpnli1yhuhAbjvAgEQgCQLhk1sfEcnKzVRLju9+bdCMfg3OuY/Qxz1y/OZI3bwEytsfI0="
-DECRYPTED_PASSWORD = boto3.client('kms').decrypt(CiphertextBlob=b64decode(ENCRYPTED_PASSWORD))['Plaintext']
+ENCRYPTED_SLACK_HOOK_URL = "AQICAHj49HEE8m06MDGiHk8OqJLX6IypkhfdVc1rfWY5AULrSgEYAMIwOQh7L+oWOOhm4UsQAAAApzCBpAYJKoZIhvcNAQcGoIGWMIGTAgEAMIGNBgkqhkiG9w0BBwEwHgYJYIZIAWUDBAEuMBEEDPqLY0VrGynYTBnl5QIBEIBgNgMJgzx5yptjBk5dBBjC9DSJSAsLDd/uDle6r/dHa453czKeFhr8bX5ML5S4NF+UN6ysmqzYeQdK0HHTdIheszvWLw76c3gpfzu9UXKaXk3B8/FspfQM72siyzZHWp19"
+DECRYPTED_SLACK_HOOK_URL = boto3.client('kms').decrypt(CiphertextBlob=b64decode(ENCRYPTED_SLACK_HOOK_URL))['Plaintext']
+SLACK_HOOK_URL = "https://%s" % DECRYPTED_SLACK_HOOK_URL
+ENCRYPTED_RAVELLO_USER = "AQICAHj49HEE8m06MDGiHk8OqJLX6IypkhfdVc1rfWY5AULrSgHZ0ERc2JNL1wKY/wYE9fKSAAAAcDBuBgkqhkiG9w0BBwagYTBfAgEAMFoGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMH52GAgIeBfjMPDKtAgEQgC3jawZ/yi9KRFlUXfyB+uwZleA/gq38j1oCqCWktrasXAgw9OB3SpNcvhyCVms="
+DECRYPTED_RAVELLO_USER = boto3.client('kms').decrypt(CiphertextBlob=b64decode(ENCRYPTED_RAVELLO_USER))['Plaintext']
+ENCRYPTED_RAVELLO_PASSWORD = "AQICAHj49HEE8m06MDGiHk8OqJLX6IypkhfdVc1rfWY5AULrSgEhzJYYnEQfpE2j0J2LlMQpAAAAZzBlBgkqhkiG9w0BBwagWDBWAgEAMFEGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMH+wpnli1yhuhAbjvAgEQgCQLhk1sfEcnKzVRLju9+bdCMfg3OuY/Qxz1y/OZI3bwEytsfI0="
+DECRYPTED_RAVELLO_PASSWORD = boto3.client('kms').decrypt(CiphertextBlob=b64decode(ENCRYPTED_RAVELLO_PASSWORD))['Plaintext']
 
 
 def post_to_slack(msg):
@@ -56,7 +56,7 @@ def post_to_slack(msg):
         'channel': SLACK_CHANNEL,
         'text': msg
     }
-    req = Request(HOOK_URL, json.dumps(slack_message))
+    req = Request(SLACK_HOOK_URL, json.dumps(slack_message))
     try:
         response = urlopen(req)
         response.read()
@@ -76,7 +76,7 @@ def handler(event, context):
     c.setopt(c.URL, 'https://cloud.ravellosystems.com/api/v1/applications')
     c.setopt(c.WRITEFUNCTION, buf.write)
     c.setopt(c.HTTPHEADER, ['Accept: application/json'])
-    c.setopt(c.USERPWD, "%s:%s" % (DECRYPTED_USER,DECRYPTED_PASSWORD))
+    c.setopt(c.USERPWD, "%s:%s" % (DECRYPTED_RAVELLO_USER,DECRYPTED_RAVELLO_PASSWORD))
     c.perform()
     j = json.loads(buf.getvalue())
     for item in j:
